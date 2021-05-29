bbb662c58aee61caa2d70e3f8e0ee9d487cdb8e2
Grogory Morozov <morozehc@yandex.ru>
2021-05-29 20:22:57 +0300

    5. creating 3 page

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// const inframe = document.querySelector(".inframe-video")
+
+
+// class ElementStyle{
+//     constructor(selector,color){
+
+//         this.selector = selector;
+//         this.color = color
+
+//         this.toChange()
+
+//     }
+
+//     toChange(){
+//         this.selector.style.background = color;
+//         alert("it done!")
+//     }
+// }
+
+
+// const style = new ElementStyle(inframe,"blue")
